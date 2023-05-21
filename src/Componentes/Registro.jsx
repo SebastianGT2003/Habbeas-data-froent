77d6f917cb3fddabae4a6fe368ea0c526648d064
sebastian gonzalez trujillo <sebastiangonza1505@hotmail.com>
2023-05-21 15:38:44 -0500

    first commit

---
+++ src/Componentes/Registro.jsx
@@ -0,0 +1,218 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import Modal from "./TermsOfUse";
+
+function Registro() {
+  //Hokks
+  const [nombre, setNombre] = useState("");
+  const [fecha, setFecha] = useState("");
+  const [isChecked, setIsChecked] = useState(false);
+  const [correo, setCorreo] = useState("");
+  const [contraseña, setContraseña] = useState("");
+  const [celular, setCelular] = useState("");
+  const [aceptaterminos, setAceptaterminos] = useState(false);
+  const [tipo_documento, setTipo_documento] = useState("");
+  const [numerodoc, setNumerodoc] = useState("");
+
+  ////
+  var today = new Date();
+
+// get today's date in `MM/DD/YYYY` format
+  var now = today.toLocaleDateString('en-US');
+  
+  const navegador = useNavigate();
+
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked);
+    setAceptaterminos(!aceptaterminos);
+  };
+
+  function registrarusuario(event) {
+    event.preventDefault();
+    var usuario = {
+      nombre: nombre,
+      tipo_documento:tipo_documento,
+      numerodoc:numerodoc,
+      fecha_n: fecha,
+      celular: celular,
+      correo: correo,
+      contraseña: contraseña,
+      aceptaterminos:aceptaterminos,
+      fecha_actual:now
+
+    };
+    console.log(usuario);
+
+    axios
+      .post("/api/usuario/registrar", usuario)
+      .then((res) => {
+        if (res.data === "Usuario registrado correctamente") {
+          alert(res.data);
+          navegador("/");
+        } else if (res.data === "El usuario ya existe") {
+          alert(res.data);
+        }
+      })
+      .then((err) => {
+        console.log(err);
+      });
+  }
+
+  return (
+    <div
+      className="modal modal-signin position-static d-block py-5"
+      tabindex="-1"
+      role="dialog"
+      id="modalSignin"
+    >
+      <div className="modal-dialog" role="document">
+        <div className="modal-content rounded-4 shadow">
+          <div className="modal-header p-5 pb-4 border-bottom-0">
+            <h1 className="text-danger fw-bold mb-0 fs-2">Nuevo usuario</h1>
+            <Link to="/">
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </Link>
+          </div>
+          <div className="modal-body p-5 pt-0">
+            <form className="needs-validation" onSubmit={registrarusuario}>
+              <div className="form-floating mb-3">
+                <input
+                  type="text"
+                  className="form-control rounded-3"
+                  id="floatingInput"
+                  placeholder="Nombre"
+                  value={nombre}
+                  onChange={(e) => {
+                    setNombre(e.target.value);
+                  }}
+                  required
+                />
+                <label for="floatingPassword">Nombre</label>
+              </div>
+              <div className="form-floating mb-3">
+                <select
+                  class="form-select"
+                  id="tipo_documento"
+                  name="tipo_documento"
+                  value={tipo_documento}
+                  onChange={(e) => {
+                    setTipo_documento(e.target.value);
+                  }}
+                  required
+                  aria-label="Tipo de documento"
+                >
+                  <option selected disabled value="">
+                    Tipo de documento
+                  </option>
+                  <option value="cc">CC - Cédula de ciudadanía</option>
+                  <option value="ce">CE - Cédula de extranjería</option>
+                  <option value="pa">PA - Pasaporte</option>
+                </select>
+
+                <label for="floatingPassword">Tipo de documento</label>
+              </div>
+              <div className="form-floating mb-3">
+                <input
+                  type="text"
+                  className="form-control rounded-3"
+                  id="floatingInput"
+                  placeholder="Numero de doc"
+                  value={numerodoc}
+                  onChange={(e) => {
+                    setNumerodoc(e.target.value);
+                  }}
+                  required
+                />
+                <label for="floatingPassword">Numero de documento</label>
+              </div>
+              <div className="form-floating mb-3">
+                <input
+                  type="date"
+                  className="form-control rounded-3"
+                  id="floatingInput"
+                  placeholder="Fecha de nacimiento"
+                  value={fecha}
+                  onChange={(e) => {
+                    setFecha(e.target.value);
+                  }}
+                  required
+                />
+
+                <label for="floatingPassword">Fecha de nacimiento</label>
+              </div>
+              <div className="form-floating mb-3">
+                <input
+                  type="number"
+                  className="form-control rounded-3"
+                  id="floatingCelular"
+                  placeholder="Celular"
+                  value={celular}
+                  onChange={(e) => {
+                    setCelular(e.target.value);
+                  }}
+                  required
+                />
+                <label for="floatingInput">Celular</label>
+              </div>
+
+              <div className="form-floating mb-3">
+                <input
+                  type="email"
+                  className="form-control rounded-3"
+                  id="floatingInput"
+                  placeholder="nombre@ejemplo.com"
+                  value={correo}
+                  onChange={(e) => {
+                    setCorreo(e.target.value);
+                  }}
+                  required
+                />
+                <label for="floatingInput">Correo</label>
+              </div>
+              <div className="form-floating mb-3">
+                <input
+                  type="password"
+                  className="form-control rounded-3"
+                  id="floatingPassword"
+                  placeholder="Contraseña"
+                  value={contraseña}
+                  onChange={(e) => {
+                    setContraseña(e.target.value);
+                  }}
+                  required
+                />
+                <label for="floatingPassword">Contreseña</label>
+              </div>
+
+              
+              <div >
+                <input
+                  type="checkbox"
+                  checked={isChecked}
+                  onChange={handleCheckboxChange}
+                />{" "}
+                Acepta los términos y condiciones
+                {isChecked && <Modal />}
+              </div>
+              <button
+                type="submit"
+                className="btn btn-lg rounded-3 w-100 mb-2 btn-outline-danger"
+                disabled={!isChecked}
+              >
+                Registarme
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Registro;
---
+++ src/Componentes/TermsOfUse.jsx
@@ -0,0 +1,174 @@
+import React, { useEffect, useState } from "react";
+import "../Estilos/Componentes.css";
+function App() {
+  //😔🤩🫡
+  const [data, setData] = useState(0);
+  const [emoji, setEmoji] = useState("😔");
+
+  useEffect(() => {
+    if (data == 0) {
+      setEmoji("😔");
+    } else if (data == 50) {
+      setEmoji("🫡");
+    } else if (data == 100) {
+      setEmoji("🤩");
+    }
+  }, [data]);
+  const [show, setShow] = useState(true);
+
+  const handleClose = () => {
+    setShow(false);
+  };
+
+  return (
+    <div>
+      {show && (
+        <div className="position- absolute top-50 start-50 translate-middle">
+           <div
+          className="modal-dialog modal-xl"
+          style={{
+            height:"auto",
+            width:"120vh",
+            display:"flex"
+          }}
+        >
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title h4" id="exampleModalFullscreenLabel">
+                Terms Of Use OFFCORSS
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                onClick={handleClose}
+              ></button>
+            </div>
+            <div className="modal-body">
+              <div className="btn-group" style={{ width: "100%" }}>
+                <button
+                  type="button"
+                  className="btn btn-danger dropdown-toggle"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                  style={{ width: "100%" }}
+                >
+                  ¿Con que frecuencia desea conocer sobre las distintas
+                  promociones y nuevo contenido que ofrece la empresa?
+                </button>
+
+                <div
+                  className="dropdown-menu p-4 text-body-secondary"
+                  style={{
+                    width: "100%",
+                    backgroundColor: "#F87878",
+                    margin: "0 auto",
+                    textAlign: "center",
+                  }}
+                >
+                  <p>
+                    Acepta recibir de vez en cuando nuestros mensajes y
+                    materiales de promoción, por correo postal, correo
+                    electrónico o cualquier otro formulario de contacto que nos
+                    proporciones (incluido tu número de teléfono para llamadas o
+                    mensajes de texto). Si no deseas recibir dichos materiales o
+                    avisos de promociones, simplemente avísanos en cualquier
+                    momento.
+                  </p>
+                  <label htmlFor="temp">
+                    {" "}
+                    Selecciona la frecuencia en la que deseas recibir nuestros
+                    mensajes:
+                  </label>
+                  <br />
+                  <div style={{ fontSize: "70px" }}>{emoji}</div>
+
+                  <input
+                    type="range"
+                    style={{ width: "40%", margin: "auto" }}
+                    className={data > 50 ? "heigh" : "less"}
+                    list="markers"
+                    min={0}
+                    max={100}
+                    step={50}
+                    value={data}
+                    onChange={(e) => setData(e.target.value)}
+                    id="temp"
+                    name="temp"
+                  />
+
+                  <datalist className="datalist" id="markers">
+                    <option className="option" value="0" label="Poco" />
+                    <option className="option" value="50" label="Moderado" />
+                    <option className="option" value="100" label="Frecuente" />
+                  </datalist>
+                </div>
+              </div>
+
+              <div className="btn-group" style={{ width: "100%" }}>
+                <button
+                  type="button"
+                  style={{ backgroundColor: "#00a0a0" }}
+                  className="btn dropdown-toggle"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  ¿Cómo usamos tu información ?
+                </button>
+
+                <div
+                  className="dropdown-menu p-4 text-body-secondary"
+                  style={{
+                    width: "100%",
+                    backgroundColor: "#00fafa",
+                    margin: "0 auto",
+                    textAlign: "center",
+                  }}
+                >
+                  <p>
+                    Usamos la información que recopilamos para ofrecerte una
+                    experiencia personalizada, incluidos anuncios, además de los
+                    otros fines que se detallan más adelante.
+                  </p>{" "}
+                  <br />
+                  <p>
+                    {" "}
+                    Para ofrecer servicios de medición, de análisis y
+                    comerciales: Mucha gente confía en nuestros Productos para
+                    dirigir o promover sus negocios. Los ayudamos a medir qué
+                    tan bien están funcionando sus anuncios y otro contenido.
+                    Para comunicarnos contigo: Nos comunicamos contigo mediante
+                    los datos que nos proporcionaste, como la información de
+                    contacto que ingresaste en tu perfil. Para realizar
+                    investigaciones e innovar para el bienestar social:
+                    Utilizamos la información que tenemos, la información de
+                    investigadores y conjuntos de datos de fuentes públicas,
+                    grupos profesionales y grupos sin fines de lucro para
+                    realizar y respaldar la investigación.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                onClick={handleClose}
+              >
+                {" "}
+                Acepto términos y condiciones
+              </button>
+            </div>
+          </div>
+        </div>
+
+
+        </div>
+       
+      )}
+
+    </div>
+      
+  );
+}
+
+export default App;
